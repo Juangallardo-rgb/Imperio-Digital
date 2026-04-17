8a3bba2cc477439b355f8e1f105c927336302d39
Juan <juanestebangallardo@hotmail.com>
2026-04-17 01:16:31 -0500

    Agregar dominio de Vercel a CORS

--- SimuladorApi/Program.cs
+++ SimuladorApi/Program.cs
@@ -87,11 +87,8 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Middleware
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
