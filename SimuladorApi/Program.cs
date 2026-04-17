123a80d10da262ee1c14bfb7f66de71f64f39a21
Juan <juanestebangallardo@hotmail.com>
2026-04-17 01:05:53 -0500

    Agregar dominio de Vercel a CORS

--- SimuladorApi/Program.cs
+++ SimuladorApi/Program.cs
@@ -58,7 +58,7 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy
-                .WithOrigins("http://localhost:5173", "http://localhost:5175", "http://localhost:5174")
+                .WithOrigins("http://localhost:5173", "http://localhost:5175", "http://localhost:5174", "https://imperio-digital-one.vercel.app")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
